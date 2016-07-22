566c1ea548fe457185723a182d33a4ea8c65789f
James Carnegie <me@kipz.org>
2016-07-22 16:04:33 +0100

    Actually don't tag PRs

--- travis-build-deploy.sh
+++ travis-build-deploy.sh
@@ -17,7 +17,7 @@ echo "Version is: $PV"
 ./lein do clean, test, jar || die "Error building/deploying jar"
 echo "Branch is ${TRAVIS_BRANCH}"
 
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST}" == "true" ]; then
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   ./lein deploy || die "Error deploying"
   git config --global user.email "travis-ci@atomist.com"
   git config --global user.name "Travis CI"
